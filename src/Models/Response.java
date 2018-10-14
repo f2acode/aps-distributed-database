5c1a92856f0d4035b9cd2699bfb59aad9b2244b9
Andrew <a.siquieri@hotmail.com>
2018-10-14 18:48:41 -0300

    ajuste response

--- src/Models/Response.java
+++ src/Models/Response.java
@@ -5,10 +5,20 @@ import java.io.Serializable;
 public class Response implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private Person person;
     private Status status;
 
-    public Response(Status status) {
+    public Response(Person person, Status status) {
+        this.person = person;
         this.status = status;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
     }
 
     public Status getStatus() {
