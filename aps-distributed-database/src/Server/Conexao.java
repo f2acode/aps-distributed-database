945aa80e53b28fb3a4cc7a74b40607bcf6ab9386
Andrew <a.siquieri@hotmail.com>
2018-10-14 16:22:43 -0300

    add files eclipse

---
+++ aps-distributed-database/src/Server/Conexao.java
@@ -0,0 +1,29 @@
+package Server;
+
+import java.net.*;
+import java.io.*;
+
+public class Conexao {
+
+    public static void send(Socket socket, Object dados) {
+        ObjectOutputStream out;
+        try {
+            out = new ObjectOutputStream(socket.getOutputStream());
+            out.writeObject(dados);
+        } catch (Exception e) {
+            System.out.println("Excecao no OutputStream");
+        }
+    }
+
+    public static Object receive(Socket socket) {
+        ObjectInputStream in;
+        Object obj=null;
+        try {
+            in = new ObjectInputStream(socket.getInputStream());
+            obj = in.readObject();
+        } catch (Exception e) {
+            System.out.println("Excecao no InputStream: " + e);
+        }
+        return obj;
+    }
+}
