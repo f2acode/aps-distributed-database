1cc21760e117ef9cff42e6ef42749f5963dd29c6
Andrew <a.siquieri@hotmail.com>
2018-10-14 16:51:43 -0300

    person commit

---
+++ src/Models/Person.java
@@ -0,0 +1,62 @@
+package Models;
+
+public class Person {
+
+    private String name;
+
+    private int age;
+
+    private Sex sex;
+
+    private String address;
+
+    private String phone;
+
+    private Person(String name, int age, Sex sex, String address, String phone) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ src/Models/Sex.java
@@ -0,0 +1,8 @@
+package Models;
+
+public enum Sex {
+
+    MALE,
+    FEMALE;
+
+}
