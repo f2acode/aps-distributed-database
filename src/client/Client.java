e65460cbf1d67b62849b19b47b6739ef525f5038
Andrew <a.siquieri@hotmail.com>
2018-11-03 20:31:23 -0300

    Mudanças no ciclo de funcionamento de requisições

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,6 @@
 package client;
 
 import models.Request;
-import models.Response;
 import models.Status;
 import models.Type;
 import models.Person;
@@ -11,8 +10,8 @@ import models.Gender;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
-
-import helpers.Connection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Client {
 
@@ -41,102 +40,76 @@ public class Client {
 
         Scanner in = new Scanner(System.in);
 
-        new Client();
+//        new Client();
         Person person;
+
+        long id;
+        String name;
+        int age;
+        Gender gender;
+        String address;
+
         Request request;
-        Type operacaoType;
+        Type type;
         Status status = null;
         do {
             String operacao = in.next();
-            operacaoType = null;
+            Pattern pattern = null;
+            Matcher matcher = null;
             switch (operacao) {
                 case "1":
                     System.out.println("\n----- Você escolheu consultar registro! -----\n");
-                    operacaoType = Type.GET;
+                    type = Type.GET;
 
-                    String id;
-                    do {
-                        status = null;
-                        System.out.println("Informe o ID do registro a ser consultado:\n");
+                    //obter dados de entrada
+                    id = getId(in, pattern, matcher, "Informe o ID do registro a ser consultado:\n");
 
-                        id = in.next();
-                        if (id.matches(".*\\w+.*")) {
-                            status = Status.ID_WRONG_FORMAT;
-                            System.out.println("Formato do ID inválido, tente novamente!\n");
-                        }
-                    } while (status != null);
-
-                    person = new Person(Long.parseLong(id));
-                    request = new Request(operacaoType, person);
-                    Connection.send(socket, request);
+                    person = new Person(id);
+                    request = new Request(type, person);
+//                    Connection.send(socket, request);
                     //                    Response response = (Response) Connection.receive(socket);
                     break;
                 case "2":
                     System.out.println("\n------ Você escolheu inserir registro! ------\n");
-                    operacaoType = Type.POST;
+                    type = Type.POST;
 
-                    String nome;
-                    String idade;
-                    String genero;
-                    Gender gender = null;
-                    String endereco;
-                    status = null;
-
+                    //obter dados de entrada
                     System.out.println("Informe os dados da pessoa a ser cadastrada:\n");
+                    name = getName(in, pattern, matcher);
+                    age = getAge(in, pattern, matcher);
+                    gender = getGender(in);
+                    address = getAddress(in);
 
-                    do {
-                        System.out.println("Nome: ");
-                        nome = in.next();
-
-
-                        if (nome.matches(".*\\d+.*")) {
-                            status = Status.NOT_A_PERSON;
-                            System.out.println("Formato de nome inválido, tente novamente!\n");
-                        }
-                    } while (status != null);
-
-                    do {
-                        System.out.println("\nIdade: ");
-                        idade = in.next();
-
-                        if (idade.matches(".*\\w+.*")) {
-                            status = Status.NOT_A_PERSON;
-                            System.out.println("Formato de idade inválida, tente novamente!\n");
-                        }
-                    } while (status != null);
-
-                    do {
-                        System.out.println("\nGênero: " +
-                                "\n\t1 - Masculino" +
-                                "\n\t2 - Feminino");
-                        genero = in.next();
-
-                        if (!(genero.equals("1") || genero.equals("2"))) {
-                            status = Status.NOT_A_PERSON;
-                            System.out.println("Formato de gênero inválido, tente novamente!\n");
-                        } else {
-                            if (genero.equals("1")) {
-                                gender = Gender.MALE;
-                            } else {
-                                gender = Gender.FEMALE;
-                            }
-                        }
-                    } while (status != null);
-
-                    System.out.println("\nEndereço: ");
-                    endereco = in.next();
-
-                    request = new Request(operacaoType, new Person(nome, Integer.getInteger(idade), gender, endereco));
-                    Connection.send(socket, request);
+                    request = new Request(type, new Person(name, age, gender, address));
+//                    Connection.send(socket, request);
                     //                    Response response = (Response) Connection.receive(socket);
                     break;
                 case "3":
                     System.out.println("\n------ Você escolheu alterar registro! ------\n");
-                    operacaoType = Type.PUT;
+                    type = Type.PUT;
+
+                    //obter dados de entrada
+                    System.out.println("Informe os dados da pessoa a ser alterada:\n");
+                    id = getId(in, pattern, matcher, "ID :\n");
+                    name = getName(in, pattern, matcher);
+                    age = getAge(in, pattern, matcher);
+                    gender = getGender(in);
+                    address = getAddress(in);
+
+                    request = new Request(type, new Person(id ,name, age, gender, address));
+//                    Connection.send(socket, request);
+                    //                    Response response = (Response) Connection.receive(socket);
                     break;
                 case "4":
                     System.out.println("\n------ Você escolheu deletar registro! ------\n");
-                    operacaoType = Type.DELETE;
+                    type = Type.DELETE;
+
+                    //obter dados de entrada
+                    id = getId(in, pattern, matcher, "Informe o ID do registro a ser deletado:\n");
+
+                    request = new Request(type, new Person(id));
+//                    Connection.send(socket, request);
+                    //                    Response response = (Response) Connection.receive(socket);
                     break;
                 case "5":
                     System.out.println("\nObrigado por utilizar nossos serviços!");
@@ -154,5 +127,83 @@ public class Client {
             System.out.println("Nao encerrou a conexao corretamente" + e.getMessage());
         }
         System.exit(0);
+    }
+
+    private static long getId(Scanner in, Pattern pattern, Matcher matcher, String message) {
+        Status status;
+        String id;
+        do {
+            status = null;
+            System.out.println(message);
+            id = in.next();
+
+            pattern = Pattern.compile("[a-zA-Z]+.*|.*[a-zA-Z]+");
+            matcher = pattern.matcher(id);
+            if (matcher.matches() || id.trim().isEmpty()) {
+                status = Status.ID_WRONG_FORMAT;
+                System.out.println("Formato do ID inválido, tente novamente!\n");
+            }
+        } while (status != null);
+        return Long.parseLong(id);
+    }
+
+    private static String getName(Scanner in, Pattern pattern, Matcher matcher) {
+        Status status;
+        String nome;
+        do {
+            status = null;
+            System.out.println("Nome: ");
+            nome = in.next();
+
+            pattern = Pattern.compile(".*\\d.*");
+            matcher = pattern.matcher(nome);
+            if (matcher.matches() || nome.trim().isEmpty()) {
+                status = Status.NOT_A_PERSON;
+                System.out.println("Formato de nome inválido, tente novamente!\n");
+            }
+        } while (status != null);
+        return nome;
+    }
+
+    private static Integer getAge(Scanner in, Pattern pattern, Matcher matcher) {
+        Status status;
+        String idade;
+        do {
+            status = null;
+            System.out.println("\nIdade: ");
+            idade = in.next();
+
+            pattern = Pattern.compile("[a-zA-Z]+.*|.*[a-zA-Z]+");
+            matcher = pattern.matcher(idade);
+            if (matcher.matches() || idade.trim().isEmpty()) {
+                status = Status.NOT_A_PERSON;
+                System.out.println("Formato de idade inválida, tente novamente!\n");
+            }
+        } while (status != null);
+        return Integer.parseInt(idade);
+    }
+
+    private static Gender getGender(Scanner in) {
+        Status status;
+        String genero;
+        do {
+            status = null;
+            System.out.println("\nGênero: " +
+                    "\n\t1 - Masculino" +
+                    "\n\t2 - Feminino");
+            genero = in.next();
+
+            if (!(genero.equals("1") || genero.equals("2")) || genero.trim().isEmpty()) {
+                status = Status.NOT_A_PERSON;
+                System.out.println("Formato de gênero inválido, tente novamente!\n");
+            }
+        } while (status != null);
+        return (genero.equals("1")) ? Gender.MALE : Gender.FEMALE;
+
+    }
+
+    private static String getAddress(Scanner in) {
+        System.out.println("\nEndereço: ");
+        return in.next();
     }
 }
